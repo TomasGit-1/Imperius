c29dcf0680ebaad80ebf3e273fd7e4fe30eed8b0
TomasGit-1 <TomasLopezPerez107@gmail.com>
2021-08-31 20:47:16 -0500

    Se implementa la clase Imagen con metodos de guardado , escala de grises y base&4

--- my_app/Api/Imagen.py
+++ my_app/Api/Imagen.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from Controller.utilities import cargaConfig , nameRandom
 import os
+import base64 
 
 class Imagen():
     configuration = cargaConfig()
@@ -8,8 +9,21 @@ class Imagen():
         try:
             id = nameRandom(5)
             img = Image.open(ruta)
+            ruta_img_Original = os.getcwd() + self.configuration["general"][0]["Imagenes"]+id+".jpg"
             imgGray = img.convert('L')
-            imgGray.save(os.getcwd() + self.configuration["general"][0]["Imagenes"]+id+".jpg")
-            print(os.getcwd() + self.configuration["general"][0]["Imagenes"]+id+".jpg")
+            imgGray.save(ruta_img_Original)
+            image_64_encode = self.Base64(ruta_img_Original)
+            return image_64_encode
         except Exception as e:
             print(str(e))
+
+    def Base64(self , ruta):
+        try:
+            image_64_encode =""
+            with open(ruta, "rb") as image_file:
+                image_64_encode = base64.b64encode(image_file.read()).decode('utf-8')
+            image_64_encode = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(image_64_encode)
+            return image_64_encode
+        except Exception as e:
+            print(str(e))
+
--- my_app/url.py
+++ my_app/url.py
@@ -5,6 +5,8 @@ from Api.Imagen import Imagen
 
 api_Imperius = Blueprint('api_Imperius' , __name__)
 configuration = cargaConfig()
+__ruta_Img_Original =""
+objImagen = Imagen()
 
 @api_Imperius.route("/")
 def home():
@@ -17,18 +19,23 @@ def Home_App():
 
 @api_Imperius.route("/cargar_image", methods = ['POST'])
 def cargar_image():
+    __ruta_Img_Original = ""
     id = nameRandom(5)
     if request.method == "POST":
         file = request.files['file']
         try:
-            ruta_Img_Original = os.getcwd() + configuration["general"][0]["Imagenes"]+id+file.filename
-            file.save(ruta_Img_Original)
-            objImagen = Imagen()
-            objImagen.Escala_Grises(ruta_Img_Original)
-            return "Imagen guardada"
+            __ruta_Img_Original = os.getcwd() + configuration["general"][0]["Imagenes"]+id+file.filename
+            file.save(__ruta_Img_Original)
+            image_64_encode = objImagen.Base64(__ruta_Img_Original)
+            return {"img_Orginal" : image_64_encode}
         except FileNotFoundError:
             return "Folder no existe"
             print("Hola mundo")
 
     return "recibido"
+
+def Escala_Grises():
+    objImagen = Imagen()
+    objImagen.Escala_Grises(__ruta_Img_Original)
+
 
